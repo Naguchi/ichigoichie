1006a5c1b14b6ae400197c63d242372aa8278844
Naguchi <lucrerakuri@gmail.com>
2015-12-13 05:24:26 +0900

    ともだち追加

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<base href="./">
 	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 	<title>一期一会</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="./css/style.css" />
 	<script src="./js/jquery-2.1.4.js"></script>
@@ -16,9 +17,11 @@
 	include( "./php/function.php" );
 ?>
 
-
 <div id="wrap">
-<header id="head"><span class="title">一期一会</span></header>
+
+<header id="head">
+  <span class="title">一期一会</span>
+</header>
 
 <?php if (get_non_meeting_alert_count()) { ?>
 <section id="alert">
@@ -34,7 +37,10 @@
 </aside>
 -->
 
-<section id="friends">
+<section class="friends">
+	<?php include( "./php/friend_add.php" ); ?>
+</section>
+<section class="friends">
 	<?php include( "./php/friends.php" ); ?>
 </section>
 
---
+++ php/friend_add.php
@@ -0,0 +1,80 @@
+<script type="text/javascript">
+$(function() {
+	$("#btn_add_friend").click(function() {
+		if ($("#friend_name").val().length) {
+			$.ajax({
+			    url: 'php/add_friend.php',
+			    type: 'post',
+			    data: {
+					name: $("#friend_name").val(),
+					last_meeting: $("#friend_last_meeting").val(),
+					alert_day: $("#friend_alert_day").val()
+				},
+			    success: function () {
+			        console.dir("success");
+			        location.reload();
+			    },
+			    error: function () {
+			    	console.dir("error");
+			    }
+			});
+		}
+	});
+});
+</script>
+
+<div class="posCenter"><h4>■ともだち追加■</h4></div>
+
+<?php if (preg_match('/Android/', $_SERVER['HTTP_USER_AGENT'])) { ?>
+
+<form>
+<table>
+	<tr>
+		<th>名前</th><td><input autofocus type="text" id="friend_name" class="len10" value="" ></input></td>
+  </tr>
+	<tr>
+		<th>最後に会った日</th><td><input type="date" id="friend_last_meeting" class="len10" value="<?php echo date('Y-m-d'); ?>"></td>
+  </tr>
+	<tr>
+		<th>アラート</th><td><select id="friend_alert_day" class="len10">
+                      			<option value="30">１ヶ月間</option>
+                      			<option value="60">２ヶ月間</option>
+                      			<option value="180">３ヶ月間</option>
+                      			<option value="7">１週間</option>
+                      			<option value="14">２週間</option>
+                      			<option value="21">３週間</option>
+                     </select></td>
+  </tr>
+	<tr>
+		<th>登録</th><td><input type="button" id="btn_add_friend" class="len10" value="よろしく！！"></td>
+	</tr>
+</table>
+</form>
+
+<?php } else { ?>
+
+<form>
+<table>
+	<tr>
+		<th>名前</th>
+		<th>最後に会った日</th>
+		<th>アラート</th>
+		<th>登録</th>
+	</tr>
+	<tr class="posCenter">
+		<td><input type="text" id="friend_name" class="len10" value="" autofocus></input></td>
+		<td><input type="date" id="friend_last_meeting" class="len10" value="<?php echo date('Y-m-d'); ?>"></td>
+		<td><select id="friend_alert_day" class="len8">
+			<option value="30">１ヶ月間</option>
+			<option value="60">２ヶ月間</option>
+			<option value="180">３ヶ月間</option>
+			<option value="7">１週間</option>
+			<option value="14">２週間</option>
+			<option value="21">３週間</option>
+		</select></td>
+		<td><input type="button" id="btn_add_friend" class="len10" value="よろしく！！"></td>
+	</tr>
+</table>
+</form>
+
+<?php } ?>
