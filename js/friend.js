a7d627252d58fc1a3c41f142b8b53826dc69bd29
Ishida Kazuki <ishida@a.b>
2016-01-08 14:14:05 +0900

    ajax更新処理に確認ダイアログ追加

--- js/friend.js
+++ js/friend.js
@@ -1,6 +1,7 @@
 $(function() {
 	$("#btn_add_friend").click(function() {
-		if ($("#friend_name").val().length) {
+		var friend_name = $("#friend_name").val();
+		if (friend_name.length && confirm(friend_name + 'さんを登録しますか？')) {
 			$.ajax({
 				url: 'php/ajax/friend_add.php',
 				type: 'post',
@@ -17,30 +18,31 @@ $(function() {
 					console.dir("error: friend add error");
 				}
 			});
-		} else {
-			console.dir("error: no name error");
 		}
 	});
 
 	$(".btn_last_mettig_update_friend").click(function() {
-		var friend_id = $(this).attr("id");
-		var now = new Date();
-		var last_mettig = (now.getFullYear())+"-"+("0"+(now.getMonth()+1)).slice(-2)+"-"+("0"+(now.getDate())).slice(-2);
-		$.ajax({
-			url: 'php/ajax/friend_last_mettig_update.php',
-			type: 'post',
-			data: {
-				id: friend_id,
-				last_meeting: last_mettig,
-			},
-			success: function () {
-				console.dir("success");
-				location.reload();
-			},
-			error: function (msg) {
-				console.dir("error: friend add error");
-				console.dir(msg);
-			}
-		});
+		var friend_name = $(this).closest("tr").children("td")[1].children.item(0).text;
+		if (confirm(friend_name + 'さんを更新しますか？')) {
+			var friend_id = $(this).attr("id");
+			var now = new Date();
+			var last_mettig = (now.getFullYear())+"-"+("0"+(now.getMonth()+1)).slice(-2)+"-"+("0"+(now.getDate())).slice(-2);
+			$.ajax({
+				url: 'php/ajax/friend_last_mettig_update.php',
+				type: 'post',
+				data: {
+					id: friend_id,
+					last_meeting: last_mettig,
+				},
+				success: function () {
+					console.dir("success");
+					location.reload();
+				},
+				error: function (msg) {
+					console.dir("error: friend add error");
+					console.dir(msg);
+				}
+			});
+		}
 	});
 });
