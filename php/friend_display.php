beaff251a458a3fbaa2a98ce6235594a1d128dc2
Ishida Kazuki <ishida@a.b>
2016-01-31 06:33:55 +0900

    エラー修正

--- php/friend_display.php
+++ php/friend_display.php
@@ -10,10 +10,10 @@ $friend_list = get_friend_list(true);
 $list = 0;
 while ($list <= count($friend_list)) { ?>
 	<tr>
-<?php for ($i=0; $i<3; $i++) { ?>
+<?php for ($i=0; $i<3; $i++, $list++) { if (isset($friend_list[$list])) { ?>
 		<th><span class="len6"><a href="./?id=<?php echo $friend_list[$list]["id"]; ?>"><?php echo $friend_list[$list]["name"]; ?></a></span></th>
-		<th><span class="len3"><?php echo ($friend_list[$list++]["display_flag"]) ? "表示" : "非表示"; ?></span></th>
-<?php } ?>
+		<th><span class="len3"><?php echo ($friend_list[$list]["display_flag"]) ? "表示" : "非表示"; ?></span></th>
+<?php } } ?>
 	</tr>
 <?php } ?>
 </table>
